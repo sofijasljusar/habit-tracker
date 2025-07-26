53330be9166d9e6cfc18d5823c323781c829d275
sofijasljusar <sofija.sljusar@gmail.com>
2025-07-26 23:55:42 +0300

    Project 8: Add UI login page

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class HomeView(TemplateView):
-    template_name = "day-detail-page.html"
+    template_name = "signup.html"

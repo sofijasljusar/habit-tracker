c6a9552bec3d33241b807788e225e2e7282be22b
sofijasljusar <sofija.sljusar@gmail.com>
2025-08-03 20:00:55 +0300

    Project8: Create Habit and HabitRecord models

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -20,3 +20,20 @@ class ToDoItem(models.Model):
 
     def __str__(self):
         return f"{self.title} ({'✓' if self.completed else '✗'})"
+
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="habits")
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class HabitRecord(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name="records")
+    date = models.DateField()
+
+    class Meta:
+        unique_together = ('habit', 'date')
+        ordering = ['date']

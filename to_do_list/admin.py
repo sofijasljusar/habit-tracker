b1e4c5f416f4d6271199d7f9e6402eb09134d73c
sofijasljusar <sofija.sljusar@gmail.com>
2026-06-01 23:12:16 +0300

    Add admin models for inspection.

--- to_do_list/admin.py
+++ to_do_list/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 
-from .models import HabitTrackingMonth, UserProfile
+from .models import UserProfile, Habit, HabitTrackingMonth, HabitRecord
+
+
+admin.site.register(UserProfile)
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ("name", "user")
+    search_fields = ("name", "user__username")
 
 
 @admin.register(HabitTrackingMonth)
 class HabitTrackingMonthAdmin(admin.ModelAdmin):
     list_display = ("habit", "year", "month")
     list_filter = ("habit", "year", "month")
+    search_fields = ("habit__name",)
+    ordering = ("-year", "-month")
 
 
-admin.site.register(UserProfile)
+@admin.register(HabitRecord)
+class HabitRecordAdmin(admin.ModelAdmin):
+    list_display = ("habit", "date")
+    list_filter = ("date",)
